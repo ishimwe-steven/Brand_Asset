73735ff7ddb6720a4203fcff338b4f03479b0a10
ishimwe-steven <157971181+ishimwe-steven@users.noreply.github.com>
2026-07-30 23:15:55 +0200

    Improve frontverfy

--- frontend/src/services/verification.service.js
+++ frontend/src/services/verification.service.js
@@ -4,3 +4,7 @@ export const getVerification = async (id) => {
   const response = await api.get(`/verifications/${id}`);
   return response.data;
 };
+export const getVerificationHistory = async () => {
+  const response = await api.get("/verifications/history");
+  return response.data;
+};
